9711e86941faf62f55091bc5f922da0acbacbb5c
Seth Messer <seth.messer@gmail.com>
2011-12-22 23:32:12 -0600

    added first helper.. title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	
+	# REturn a title on a per-page basis.
+	def title
+		base_title = "devel"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+
 end
